87dd3e5e21a32fa7b0d187ea3b0c4a748b97797a
Gary Byrne <gary.byrne39@mail.dcu.ie>
2016-12-16 17:16:24 +0000

    Purify unused css

---
+++ config.rb
@@ -0,0 +1,7 @@
+http_path = '/'
+css_dir = 'app/static/css'
+sass_dir = 'app/static/sass'
+images_dir = 'app/static/img'
+javascripts_dir = 'app/static/js'
+relative_assets = true
+line_comments = true
